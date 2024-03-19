c59583f360c9321a745696ddc4e34bc8bf3c20ce
JohnMutemi <jmtutorsalp@gmail.com>
2024-03-19 15:12:41 +0300

    commit arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply=(num1*num2);
+console.log(multiply);
+
+const random=Math.floor((Math.random()*10) + 1); 
+console.log(random);
+const num3=28;
+const num4=6;
+const mod=(num3%num4);
+console.log(mod);
+
+const set=[10,14,6,8,2,20,19];
+const max=Math.max(...set);
+console.log(max);
